b20d881132eeac07ad2bc8e159e3b37611be4f6c
Niranjan Phatak <niru.phatak@gmail.com>
2020-10-26 13:25:40 +0530

    Spring Cloud Circuit Breaker With Netflix Implementation

--- src/main/java/com/experience/api/ExampleRestController.java
+++ src/main/java/com/experience/api/ExampleRestController.java
@@ -1,24 +1,32 @@
 package com.experience.api;
 
 import org.apache.commons.lang3.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/experience/api")
 public class ExampleRestController {
 
+	@SuppressWarnings("rawtypes")
+	@Autowired
+	private CircuitBreakerFactory circuitBreakerFactory;
+
 	@GetMapping("/account/summary")
-	@HystrixCommand(fallbackMethod = "getAccountSummaryFallback", commandKey = "accountsummary", groupKey = "accountsummary")
 	public String getAccountSummary() {
 
-		if (RandomUtils.nextBoolean()) {
-			throw new RuntimeException("Exception in Account Summary");
-		}
-		return "AccountSummry";
+		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("circuitbreaker");
+
+		return circuitBreaker.run(() -> {
+			if (RandomUtils.nextBoolean()) {
+				throw new RuntimeException("Exception in Account Summary");
+			}
+			return "AccountSummry";
+		}, throwable -> getAccountSummaryFallback());
 
 	}
 
